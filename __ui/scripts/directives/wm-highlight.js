ba36798ae39b61851bee20a670076f96971492bf
Roger Abelenda <rabelenda@3cinteractive.com>
2014-03-10 23:28:59 -0300

    Included http and xml as highlighting syntaxes and added an option in wm-highlight to be able to set in a parameter the syntaxes to restrict the highlighting to.

--- __ui/scripts/directives/wm-highlight.js
+++ __ui/scripts/directives/wm-highlight.js
@@ -2,6 +2,9 @@ angular.module("wmHighlight",[]).directive('wmHighlight', function() {
 
   function link(scope, element, attrs) {
     scope.$watch('mappings', function(value) {
+      if (attrs.wmHighlight != "") {
+        hljs.configure({languages : attrs.wmHighlight.split(",")});
+      }
       //need to use element[0] to unwrap jquery element
       hljs.highlightBlock(element[0]);
     });
